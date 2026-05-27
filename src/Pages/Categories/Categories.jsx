f8d39686e84e18199950803f656b57f6d236fae9
Tu Nombre <diegop.salas@gmail.com>
2026-05-27 17:33:38 -0400

    fix categorias images

--- src/Pages/Categories/Categories.jsx
+++ src/Pages/Categories/Categories.jsx
@@ -2,33 +2,40 @@ import React from 'react'
 import { Link } from "react-router";
 import './Categories.css'
 
+import imgIngles from '../../assets/img/english.png'
+import imgHistoria from '../../assets/img/history.png'
+import imgMates from '../../assets/img/maths.png'
+import imgCiencias from '../../assets/img/science.png'
+import imgLenguaje from '../../assets/img/spanish.png'
+
 const Categories = () => {
     const categorias = [
-        {nombre: "Inglés", img_name: "english.png", color: "#FEF4BB", class: "ingles"},
-        {nombre: "Historia", img_name:"history.png", color: "#FEEAC0", class: "historia"},
-        {nombre: "Matemáticas", img_name: "maths.png", color: "#C0E9FE", class: "matematicas"},
-        {nombre: "Ciencias", img_name: "science.png", color: "#E5FEC0", class: "ciencias"},
-        {nombre: "Lenguaje", img_name: "spanish.png", color: "#E9CAFE", class: "lenguaje"}
+        { nombre: "Inglés",      img: imgIngles,   color: "#FEF4BB", class: "ingles"      },
+        { nombre: "Historia",    img: imgHistoria,  color: "#FEEAC0", class: "historia"    },
+        { nombre: "Matemáticas", img: imgMates,     color: "#C0E9FE", class: "matematicas" },
+        { nombre: "Ciencias",    img: imgCiencias,  color: "#E5FEC0", class: "ciencias"    },
+        { nombre: "Lenguaje",    img: imgLenguaje,  color: "#E9CAFE", class: "lenguaje"    },
     ]
-  return (
-    <div className="container myCategories py-3">
-        <h1 className="section-title display-5">Nuestras categorías</h1>
-        <div className="row row-cols-2 mt-2 row-cols-lg-3 text-center gx-lg-4">
-            {categorias.map((category)=>
-            (<div className="col">
-                <Link to={`/categorias/${category.class}`} className="card-title text-decoration-none">
-                <div className={`card mycard ${category.class}`}>
-                    <img src={`./src/assets/img/${category.img_name}`} className="card-img-top mx-auto" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">{category.nombre}</h5>
+
+    return (
+        <div className="container myCategories py-3">
+            <h1 className="section-title display-5">Nuestras categorías</h1>
+            <div className="row row-cols-2 mt-2 row-cols-lg-3 text-center gx-lg-4">
+                {categorias.map((category) => (
+                    <div className="col" key={category.class}>
+                        <Link to={`/categorias/${category.class}`} className="card-title text-decoration-none">
+                            <div className={`card mycard ${category.class}`}>
+                                <img src={category.img} className="card-img-top mx-auto" alt={category.nombre} />
+                                <div className="card-body">
+                                    <h5 className="card-title">{category.nombre}</h5>
+                                </div>
+                            </div>
+                        </Link>
                     </div>
-                </div>
-                </Link>
-            </div>)
-            )}
+                ))}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Categories
